74214e63647be9f66475bcd0cb0d0fa8af372a99
Emik <emik.cuber@gmail.com>
2025-06-30 12:18:17 +0200

    Mark sealed

--- Source/RemoteJsonSerializerContext.cs
+++ Source/RemoteJsonSerializerContext.cs
@@ -4,4 +4,4 @@ namespace Remote;
 /// <summary>Contains the context used for JSON serialization.</summary>
 [JsonSerializable(typeof(JsonNode)), JsonSerializable(typeof(List<Preferences.Connection>)),
  JsonSourceGenerationOptions(WriteIndented = true)]
-partial class RemoteJsonSerializerContext : JsonSerializerContext;
+sealed partial class RemoteJsonSerializerContext : JsonSerializerContext;
