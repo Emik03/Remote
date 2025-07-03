0af1437291af839ab95aee2a820957211da1f500
Emik <emik.cuber@gmail.com>
2025-07-03 18:28:48 +0200

    Link with file system

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,5 +1,15 @@
 ﻿// SPDX-License-Identifier: MPL-2.0
 #if !ANDROID
+AppDomain.CurrentDomain.AssemblyResolve +=
+    (_, a) => a.Name.StartsWith("Remote.Resources")
+        ? Assembly.LoadFile(
+            Path.Join(
+                Path.GetDirectoryName(typeof(RemoteGame).Assembly.Location),
+                $"{nameof(Remote)}.{nameof(Remote.Resources)}.dll"
+            )
+        )
+        : null;
+
 using RemoteGame game = new();
 game.Run();
 #endif
