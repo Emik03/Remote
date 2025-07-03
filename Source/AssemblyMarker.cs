d3cdc29806f91eb60f92dc25f4d9f85656316173
Emik <emik.cuber@gmail.com>
2025-07-03 18:28:37 +0200

    Remove file reference

--- Source/AssemblyMarker.cs
+++ Source/AssemblyMarker.cs
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: MPL-2.0
+#pragma warning disable GlobalUsingsAnalyzer
+global using PureAttribute = System.Diagnostics.Contracts.PureAttribute;
+
+#pragma warning restore GlobalUsingsAnalyzer
 namespace Remote.Resources;
 
 /// <summary>The marker for this assembly. Meant to be used for <c>typeof()</c> to bind with this assembly.</summary>
