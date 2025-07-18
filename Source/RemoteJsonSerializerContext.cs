914adacb71aadc112ffd73f00a272a91ae4b22c3
Emik <emik.cuber@gmail.com>
2025-07-18 19:00:41 +0200

    Use new data structure in serializer context

--- Source/RemoteJsonSerializerContext.cs
+++ Source/RemoteJsonSerializerContext.cs
@@ -2,6 +2,6 @@
 namespace Remote;
 
 /// <summary>Contains the context used for JSON serialization.</summary>
-[JsonSerializable(typeof(JsonNode)), JsonSerializable(typeof(List<HistoryEntry>)),
+[JsonSerializable(typeof(JsonNode)), JsonSerializable(typeof(OrderedDictionary<string, HistoryServer>)),
  JsonSourceGenerationOptions(WriteIndented = true)]
 sealed partial class RemoteJsonSerializerContext : JsonSerializerContext;
