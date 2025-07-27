574fb4262e5ef80dac2ceebef0fc35ecfbedffe0
Emik <emik.cuber@gmail.com>
2025-07-27 16:38:07 +0200

    Allow logic trimming to redirected to standard output

--- Remote.Portable/Source/ApLogicTrimmer.cs
+++ Remote.Portable/Source/ApLogicTrimmer.cs
@@ -7,7 +7,6 @@ namespace Remote.Portable;
 /// </summary>
 public static class ApLogicTrimmer
 {
-#if LOGIC_TRIM_CONSOLE_WRITE
     /// <summary>The maximum number of variables for assertions.</summary>
     /// <remarks><para>
     /// Increasing this number performs a more exhaustive search of logic with
@@ -15,9 +14,16 @@ public static class ApLogicTrimmer
     /// </para></remarks>
     const int MaxVariableCountForAssertions = 16;
 
+    /// <summary>Gets the truthy symbol.</summary>
+    const string TrueSymbol = "{True}";
+
+    /// <summary>The environment variables.</summary>
+    static readonly bool s_debugLogicTrim = IsTrue("REMOTE_DEBUG_LOGIC_TRIM"),
+        s_debugLogicTrimNull = IsTrue("REMOTE_DEBUG_LOGIC_TRIM_NULL");
+
     /// <summary>Contains every string that was printed before.</summary>
     static readonly HashSet<string> s_seen = new(FrozenSortedDictionary.Comparer);
-#endif
+
     /// <summary>Tests whether this logic is fulfilled by the set of inputs.</summary>
     /// <typeparam name="T">The type of collection for logic.</typeparam>
     /// <param name="that">The logic to test.</param>
@@ -35,45 +41,69 @@ public static class ApLogicTrimmer
             _ => TestUnary(that, lookup, inputs),
         };
 
+    /// <summary>Determines if the environment variable is true.</summary>
+    /// <param name="variable">The environment variable.</param>
+    /// <returns>Whether the parameter <paramref name="variable"/> is a truthy environment variable.</returns>
+    public static bool IsTrue(string variable) =>
+        Environment.GetEnvironmentVariable(variable) is var env && env.AsSpan().Trim() is ['1'] ||
+        bool.TryParse(env, out var result) && result;
+
     /// <summary>Returns itself, discarding the parameter. Prints both if compiled with a specific constant.</summary>
     /// <param name="left">The logic to return.</param>
     /// <param name="right">The logic to discard.</param>
     /// <param name="line">The caller line number to discard.</param>
     /// <returns>Itself.</returns>
-#if !LOGIC_TRIM_CONSOLE_WRITE
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
     [return: NotNullIfNotNull(nameof(left))]
-#pragma warning disable MA0051
     internal static ApLogic? Check(this ApLogic? left, [UsedImplicitly] ApLogic? right, [CallerLineNumber] int line = 0)
-#pragma warning restore MA0051
-#if LOGIC_TRIM_CONSOLE_WRITE
     {
-        const int Or = 120, And = Or + 32, Evaluator = 60;
-        const string OrSymbol = "+", AndSymbol = "*", TrueSymbol = "{True}";
         left?.IsOptimized = true;
 
-        static string Display(ApLogic? logic) => logic?.ToBooleanAlgebra(AndSymbol, OrSymbol) ?? TrueSymbol;
+        if (s_debugLogicTrim)
+            Debug(left, right, line);
 
-        Dictionary<int, string> dictionary = new()
+        return left;
+    }
+
+    /// <summary>Returns itself, discarding the parameter. Prints both if compiled with a specific constant.</summary>
+    /// <param name="left">The logic to return.</param>
+    /// <param name="tuple">The tuple to discard.</param>
+    /// <param name="evaluator">The evaluator to discard.</param>
+    /// <param name="line">The caller line number to discard.</param>
+    /// <returns>Itself.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [return: NotNullIfNotNull(nameof(left))]
+    internal static ApLogic? Check(
+        this ApLogic? left,
+        [UsedImplicitly] in (ApLogic? Left, ApLogic? Right) tuple,
+        [UsedImplicitly] ApEvaluator evaluator,
+        [CallerLineNumber] int line = 0
+    ) => // ReSharper disable once ExplicitCallerInfoArgument
+        Check(left, s_debugLogicTrim ? evaluator.In(tuple.Right) : null, line);
+
+    /// <summary>Performs assertions and printing.</summary>
+    /// <param name="left">The logic to return.</param>
+    /// <param name="right">The logic to discard.</param>
+    /// <param name="line">The caller line number to look up.</param>
+    static void Debug(ApLogic? left, ApLogic? right, int line)
+    {
+        const int Or = 120, And = Or + 32, Evaluator = 60;
+
+        Dictionary<int, string?> dictionary = new()
         {
             [Evaluator + 1] = "Resolving (Left) as FALSE then Annulment Law in AND",
             [Evaluator + 2] = "Resolving (Right) as FALSE then Annulment Law in AND",
             [Evaluator + 9] = "Resolving (Left) as FALSE then Identity Law in OR",
             [Evaluator + 10] = "Resolving (Right) as FALSE then Identity Law in OR",
-#if LOGIC_TRIM_NULL_CONSOLE_WRITE
-            [Or + 1] = "Identity Law (Left) in OR",
-            [Or + 2] = "Identity Law (Right) in OR",
-#endif
+            [Or + 1] = s_debugLogicTrimNull ? "Identity Law (Left) in OR" : null,
+            [Or + 2] = s_debugLogicTrimNull ? "Identity Law (Right) in OR" : null,
             [Or + 5] = "Idempotent Law in AND",
             [Or + 8] = "Commutative Law then Idempotent Law in OR",
             [Or + 11] = "Idempotent Law in OR",
             [Or + 14] = "Commutative Law then Absorption Law in OR",
             [Or + 17] = "Absorption Law in OR",
-#if LOGIC_TRIM_NULL_CONSOLE_WRITE
-            [And + 1] = "Annulment Law (Left) in AND",
-            [And + 2] = "Annulment Law (Right) in AND",
-#endif
+            [And + 1] = s_debugLogicTrimNull ? "Annulment Law (Left) in AND" : null,
+            [And + 2] = s_debugLogicTrimNull ? "Annulment Law (Right) in AND" : null,
             [And + 5] = "Idempotent Law in AND",
             [And + 8] = "Commutative Law then Absorption Law in AND",
             [And + 11] = "Absorption Law in AND",
@@ -81,26 +111,24 @@ public static class ApLogicTrimmer
             [And + 17] = "Idempotent Law in AND",
         };
 
-        if (!dictionary.TryGetValue(line, out var value))
-            return left;
+        if (!dictionary.TryGetValue(line, out var value) ||
+            value is null ||
+            $"""
+             Found optimization: {value}
+             Preserving: {left?.ToString() ?? TrueSymbol}
+             Discarding: {right?.ToString() ?? TrueSymbol}
+             In boolean algebra…
+             Preserving: {Display(left)}
+             Discarding: {Display(right)}
 
-        var key = $"""
-                   Found optimization: {value}
-                   Preserving: {left?.ToString() ?? TrueSymbol}
-                   Discarding: {right?.ToString() ?? TrueSymbol}
-                   In boolean algebra…
-                   Preserving: {Display(left)}
-                   Discarding: {Display(right)}
-
-                   """;
-
-        if (!s_seen.Add(key))
-            return left;
+             """ is var key &&
+            !s_seen.Add(key))
+            return;
 
         Console.WriteLine(key);
 
         if (left is null || right is null || line < Or)
-            return left;
+            return;
 
         IList<ApLogic> list = [];
         var combined = line >= And ? ApLogic.OfAnd(left, right) : ApLogic.OfOr(left, right);
@@ -117,40 +145,7 @@ public static class ApLogicTrimmer
            .Lazily(_ => Trace.Assert(originalCount == list.Count))
            .Lazily(x => Trace.Fail($"{x.Select(x => x ? 'X' : '_').Concat()}\n{Display(left)}\n{Display(combined)}"))
            .Enumerate();
-
-        return left;
     }
-#else
-    {
-        left?.IsOptimized = true;
-        return left;
-    }
-#endif
-    /// <summary>Returns itself, discarding the parameter. Prints both if compiled with a specific constant.</summary>
-    /// <param name="left">The logic to return.</param>
-    /// <param name="tuple">The tuple to discard.</param>
-    /// <param name="evaluator">The evaluator to discard.</param>
-    /// <param name="line">The caller line number to discard.</param>
-    /// <returns>Itself.</returns>
-#if !LOGIC_TRIM_CONSOLE_WRITE
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-    [return: NotNullIfNotNull(nameof(left))]
-    internal static ApLogic? Check(
-        this ApLogic? left,
-        [UsedImplicitly] in (ApLogic? Left, ApLogic? Right) tuple,
-        [UsedImplicitly] ApEvaluator evaluator,
-        [CallerLineNumber] int line = 0
-    )
-#if LOGIC_TRIM_CONSOLE_WRITE // ReSharper disable once ExplicitCallerInfoArgument
-        =>
-            Check(left, evaluator.In(tuple.Right), line);
-#else
-    {
-        left?.IsOptimized = true;
-        return left;
-    }
-#endif
 
     /// <inheritdoc cref="Test{T}"/>
     static bool TestUnary<T>(this ApLogic that, T lookup, ReadOnlySpan<bool> inputs)
@@ -166,5 +161,14 @@ public static class ApLogicTrimmer
 
         lookup.Add(that);
         return Index(inputs, i);
+    }
+
+    /// <summary>Displays the logic.</summary>
+    /// <param name="logic">The logic to display.</param>
+    /// <returns>The displayed logic.</returns>
+    static string Display(ApLogic? logic)
+    {
+        const string OrSymbol = "+", AndSymbol = "*";
+        return logic?.ToBooleanAlgebra(AndSymbol, OrSymbol) ?? TrueSymbol;
     }
 }
